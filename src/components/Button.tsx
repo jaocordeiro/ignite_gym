cefc326e87331b672137772912f08eae9536266b
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-09-19 22:06:56 -0300

    create routes not auth

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,22 +2,29 @@ import { Button as ButtonNativeBase, IButtonProps, Text } from "native-base";
 
 type Props = IButtonProps & {
   title: string;
+  variant?: "solid" | "outline";
 };
 
-export default function Button({ title, ...rest }: Props) {
+export default function Button({ title, variant = "solid", ...rest }: Props) {
   return (
     <ButtonNativeBase
       w="full"
       h={12}
-      bg="green.700"
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.500"
       mt={3}
       rounded="sm"
       _pressed={{
-        bg: "green.500",
+        bg: variant === "outline" ? "gray.500" : "green.500",
       }}
       {...rest}
     >
-      <Text color="white" fontFamily="body" fontSize="md">
+      <Text
+        color={variant === "outline" ? "green.500" : "white"}
+        fontFamily="body"
+        fontSize="md"
+      >
         {title}
       </Text>
     </ButtonNativeBase>
