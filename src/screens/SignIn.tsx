907354fff683f22734e73547bf6b3e7837c9294d
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-11-13 22:11:12 -0300

    inputs init controller with react hook form

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,16 +1,33 @@
 import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import { useForm, Controller } from "react-hook-form";
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
 import { Input } from "@components/Input";
 import Button from "@components/Button";
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
 export function SignIn() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormDataProps>();
 
   function handleNewAccount() {
     navigation.navigate("SignUp");
+  }
+
+  function handleSignUp(data: FormDataProps) {
+    console.log("LOG:(SignIn) - data", data);
   }
 
   return (
@@ -40,11 +57,62 @@ export function SignIn() {
           </Heading>
         </Center>
 
-        <Input placeholder="E-mail" keyboarType="email" autoCapitalize="none" />
+        <Controller
+          control={control}
+          name="name"
+          rules={{ required: "informe o nome!" }}
+          render={({ field: { onChange, value } }) => (
+            <Input placeholder="Nome" onChangeText={onChange} value={value} />
+          )}
+        />
+        <Text color="red.400">{errors.name?.message}</Text>
 
-        <Input placeholder="Senha" secureTextEntry />
+        <Controller
+          control={control}
+          name="email"
+          rules={{ required: "informe o E-mail!" }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="E-mail"
+              keyboarType="email"
+              autoCapitalize="none"
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
 
-        <Button title="Acessar" />
+        <Controller
+          control={control}
+          name="password"
+          rules={{ required: "informe a senha!" }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Senha"
+              secureTextEntry
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="confirmPassword"
+          rules={{ required: "Confirme a senha!" }}
+          render={({ field: { onChange, value } }) => (
+            <Input
+              placeholder="Confirmar Senha"
+              secureTextEntry
+              onChangeText={onChange}
+              value={value}
+              onSubmitEditing={handleSubmit(handleSignUp)}
+              returnKeyType="send"
+            />
+          )}
+        />
+
+        <Button title="Criar e Acessar" onPress={handleSubmit(handleSignUp)} />
 
         <Center mt={32}>
           <Text color="gray.100" fontSize="sm" mb={1} fontFamily="body">
