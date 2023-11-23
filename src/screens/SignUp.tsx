69b030a19cfc6281d943a5d93a474231fee5613e
jaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-11-22 21:30:35 -0300

    back-and-forth error handling

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,13 +1,25 @@
 import { useNavigation } from "@react-navigation/native";
-import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+import {
+  VStack,
+  Image,
+  Text,
+  Center,
+  Heading,
+  ScrollView,
+  useToast,
+} from "native-base";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { api } from "@services/api";
+import { Input } from "@components/Input";
+import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import axios from "axios";
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
-import { Input } from "@components/Input";
 import Button from "@components/Button";
-import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { Alert } from "react-native";
+import { AppError } from "@utils/AppError";
 
 type FormDataProps = {
   name: string;
@@ -34,6 +46,7 @@ const signUpSchema = yup.object({
 
 export function SignUp() {
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const toast = useToast();
 
   function handleGoBack() {
     navigation.navigate("SignIn");
@@ -51,20 +64,19 @@ export function SignUp() {
   }
 
   async function handleSignUp({ name, password, email }: FormDataProps) {
-    const response = await fetch("http://192.168.237.75:3333/users", {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        password,
-        email,
-      }),
-    });
-    const data = await response.json();
-    console.log("LOG:(SignUp) - data", data);
+    try {
+      const response = await api.post("/users", { name, password, email });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Nao foi possivel criar a conta, tente novamente mais tarde.";
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    }
   }
 
   return (
